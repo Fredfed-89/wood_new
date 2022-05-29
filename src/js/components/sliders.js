fbf38128ce61f2dca4f7b988674a460a71673242
Fredfed-89 <fedyaev.sergei89@gmail.com>
2022-05-29 09:53:46 +0300

    first commit

---
+++ src/js/components/sliders.js
@@ -0,0 +1,37 @@
+import Swiper from 'swiper/bundle';
+
+const newSwiper = new Swiper('.swiper', {
+  // direction: 'horizontal',
+  slidesPerView: "auto",
+  centeredSlides: false,
+  // slidesPerGroup: 1,
+  spaceBetween: 20,
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+
+    },
+
+    768: {
+      slidesPerView: 1,
+    },
+
+    992: {
+      spaceBetween: 10,
+      slidesPerView: 2,
+      // centeredSlides: true,
+    },
+
+    1170: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+
+});
